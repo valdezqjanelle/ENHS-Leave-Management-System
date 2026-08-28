b40d82065881addf7968d1dec4c6d3291328a760
Janelle V <bl9351053@gmail.com>
2026-08-29 01:56:10 +0800

    cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,22 +7,19 @@ return [
         'sanctum/csrf-cookie',
     ],
 
-    'allowed_methods' => [
-        '*',
-    ],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+
         'https://enhs-leave-management-system-woad.vercel.app',
         'https://enhs-leave-management-system-git-leave-ebc156-enhs-leave-system.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        '*',
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
