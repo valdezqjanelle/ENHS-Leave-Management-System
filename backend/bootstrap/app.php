1f3507893917b51f8f83eac4549fd7b470afb97c
Janelle V <bl9351053@gmail.com>
2026-08-29 07:16:54 +0800

    corrr

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+
+        // Laravel built-in CORS middleware
+        $middleware->prepend(HandleCors::class);
 
         $middleware->redirectGuestsTo(null);
     })
