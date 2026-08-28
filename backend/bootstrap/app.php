cda78b5939c5c5755d45894a00b63ff56d2d9e63
Janelle V <bl9351053@gmail.com>
2026-08-29 02:06:31 +0800

    bt

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -18,10 +18,16 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
 
-        // Enable CORS for all requests
-        $middleware->prepend(
-            \Illuminate\Http\Middleware\HandleCors::class
-        );
+        // CORS
+        $middleware->use([
+            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        ]);
 
         $middleware->redirectGuestsTo(null);
     })
