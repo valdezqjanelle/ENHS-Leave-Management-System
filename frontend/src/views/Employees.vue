69f54b5cd76fce80eee04c71210db57bc702962a
Janelle V <bl9351053@gmail.com>
2026-08-29 19:39:52 +0800

    fix dep

--- frontend/src/views/Employees.vue
+++ frontend/src/views/Employees.vue
@@ -298,7 +298,10 @@
                 <label class="block mb-2 text-sm text-gray-800 font-medium"
                   >Department</label
                 >
-                <select v-model="form.department_id">
+                <select v-model="form.department_id"
+                class="w-full min-w-0 border rounded-lg px-3 py-2 text-gray-800"
+                >
+                  
                   <option :value="null">Select Department</option>
 
                   <option
