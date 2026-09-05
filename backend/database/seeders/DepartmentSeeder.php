5d70f0a81ba24ebce14aa5fc63fd17ef26115dc0
Janelle V <bl9351053@gmail.com>
2026-09-05 13:31:32 +0800

    nnjj

--- backend/database/seeders/DepartmentSeeder.php
+++ backend/database/seeders/DepartmentSeeder.php
@@ -2,47 +2,91 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Department;
+use Illuminate\Database\Seeder;
 
 class DepartmentSeeder extends Seeder
 {
     public function run(): void
     {
         $departments = [
-            // JHS
-            ['department_name' => 'Filipino', 'level' => 'JHS'],
-            ['department_name' => 'English', 'level' => 'JHS'],
-            ['department_name' => 'Mathematics', 'level' => 'JHS'],
-            ['department_name' => 'Science', 'level' => 'JHS'],
-            ['department_name' => 'MAPEH', 'level' => 'JHS'],
-            ['department_name' => 'Araling Panlipunan', 'level' => 'JHS'],
-            ['department_name' => 'ESP / Values Education', 'level' => 'JHS'],
-            ['department_name' => 'TLE', 'level' => 'JHS'],
-
-            // SHS
-            ['department_name' => 'Biological Science', 'level' => 'SHS'],
-            ['department_name' => 'Physical Education', 'level' => 'SHS'],
-            ['department_name' => 'Mathematics', 'level' => 'SHS'],
-            ['department_name' => 'English', 'level' => 'SHS'],
-            ['department_name' => 'Social Science', 'level' => 'SHS'],
-            ['department_name' => 'Filipino', 'level' => 'SHS'],
-            ['department_name' => 'Araling Panlipunan', 'level' => 'SHS'],
-            ['department_name' => 'Commerce', 'level' => 'SHS'],
-            ['department_name' => 'Management Accounting', 'level' => 'SHS'],
-            ['department_name' => 'ABM', 'level' => 'SHS'],
-            ['department_name' => 'ICT', 'level' => 'SHS'],
-
-            // Non-Teaching
-            ['department_name' => 'School Administration', 'level' => 'Non-Teaching'],
-            ['department_name' => 'Senior Bookeeper', 'level' => 'Non-Teaching'],
-            ['department_name' => 'Clerk', 'level' => 'Non-Teaching'],
-            ['department_name' => 'Disbursing Officer', 'level' => 'Non-Teaching'],
-            ['department_name' => 'Utility / Job Order', 'level' => 'Non-Teaching'],
+            [
+                'department_name' => 'Filipino',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'English',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'Mathematics',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'Science',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'MAPEH',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'Araling Panlipunan',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'ESP / Values Education',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'TLE',
+                'level' => 'JHS',
+            ],
+            [
+                'department_name' => 'STEM',
+                'level' => 'SHS',
+            ],
+            [
+                'department_name' => 'HUMSS',
+                'level' => 'SHS',
+            ],
+            [
+                'department_name' => 'ABM',
+                'level' => 'SHS',
+            ],
+            [
+                'department_name' => 'TVL - ICT',
+                'level' => 'SHS',
+            ],
+            [
+                'department_name' => 'School Administration',
+                'level' => 'Non-Teaching',
+            ],
+            [
+                'department_name' => 'Administrative Services',
+                'level' => 'Non-Teaching',
+            ],
+            [
+                'department_name' => 'Finance and Disbursement',
+                'level' => 'Non-Teaching',
+            ],
+            [
+                'department_name' => 'General Services',
+                'level' => 'Non-Teaching',
+            ],
         ];
 
-        foreach ($departments as $dept) {
-            Department::create($dept);
+        foreach ($departments as $department) {
+            Department::updateOrCreate(
+                [
+                    'department_name' =>
+                        $department['department_name'],
+
+                    'level' =>
+                        $department['level'],
+                ],
+                $department
+            );
         }
     }
 }
